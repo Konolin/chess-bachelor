3ea3532ca3e3aace464e87586d7df15d4e519b78
Catalin <catalin.mandru@stud.ubbcluj.ro>
2025-03-15 16:38:40 +0200

    Refactored the testing methods

--- backend/src/test/java/com/example/backend/MoveGenerationTest.java
+++ backend/src/test/java/com/example/backend/MoveGenerationTest.java
@@ -19,8 +19,8 @@ class MoveGenerationTest {
     }
 
     long generateFollowingMovesCount(Board board, final long depth, boolean isFirstIteration, boolean isDebug) {
-        if (depth == 0) {
-            return 1;
+        if (depth == 1) {
+            return board.getAlliancesLegalMoves(board.getMoveMaker()).size();
         }
 
         MoveList legalMoves = board.getAlliancesLegalMoves(board.getMoveMaker());
@@ -45,63 +45,33 @@ class MoveGenerationTest {
     void testMoveGenerationPosition1() {
         Board board = FenService.createGameFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -");
         long numMoves;
-
-        numMoves = generateMovesCount(board, 1);
-        assertEquals(20, numMoves);
-
-        numMoves = generateMovesCount(board, 2);
-        assertEquals(400, numMoves);
-
-        numMoves = generateMovesCount(board, 3);
-        assertEquals(8902, numMoves);
-
-        numMoves = generateMovesCount(board, 4);
-        assertEquals(197281, numMoves);
-
-        numMoves = generateMovesCount(board, 5);
-        assertEquals(4865609, numMoves);
+        int[] expectedMoves = {20, 400, 8_902, 197_281, 4_865_609, 119_060_324};
+        for (int i = 0; i < expectedMoves.length; i++) {
+            numMoves = generateMovesCount(board, i + 1);
+            assertEquals(expectedMoves[i], numMoves);
+        }
     }
 
     @Test
     void testMoveGenerationPosition2() {
         Board board = FenService.createGameFromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
         long numMoves;
-
-        numMoves = generateMovesCount(board, 1);
-        assertEquals(48, numMoves);
-
-        numMoves = generateMovesCount(board, 2);
-        assertEquals(2039, numMoves);
-
-        numMoves = generateMovesCount(board, 3);
-        assertEquals(97862, numMoves);
-
-        numMoves = generateMovesCount(board, 4);
-        assertEquals(4085603, numMoves);
+        int[] expectedMoves = {48, 2039, 97_862, 4_085_603, 193_690_690};
+        for (int i = 0; i < expectedMoves.length; i++) {
+            numMoves = generateMovesCount(board, i + 1);
+            assertEquals(expectedMoves[i], numMoves);
+        }
     }
 
     @Test
     void testMoveGenerationPosition3() {
         Board board = FenService.createGameFromFEN("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ");
         long numMoves;
-
-        numMoves = generateMovesCount(board, 1);
-        assertEquals(14, numMoves);
-
-        numMoves = generateMovesCount(board, 2);
-        assertEquals(191, numMoves);
-
-        numMoves = generateMovesCount(board, 3);
-        assertEquals(2812, numMoves);
-
-        numMoves = generateMovesCount(board, 4);
-        assertEquals(43238, numMoves);
-
-        numMoves = generateMovesCount(board, 5);
-        assertEquals(674624, numMoves);
-
-        numMoves = generateMovesCount(board, 6);
-        assertEquals(11030083, numMoves);
+        int[] expectedMoves = {14, 191, 2_812, 43_238, 674_624, 11_030_083, 178_633_661};
+        for (int i = 0; i < expectedMoves.length; i++) {
+            numMoves = generateMovesCount(board, i + 1);
+            assertEquals(expectedMoves[i], numMoves);
+        }
     }
 
     @Test
@@ -109,58 +79,32 @@ class MoveGenerationTest {
         Board board = FenService.createGameFromFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq -");
         long numMoves;
 
-        numMoves = generateMovesCount(board, 1);
-        assertEquals(6, numMoves);
-
-        numMoves = generateMovesCount(board, 2);
-        assertEquals(264, numMoves);
-
-        numMoves = generateMovesCount(board, 3);
-        assertEquals(9467, numMoves);
-
-        numMoves = generateMovesCount(board, 4);
-        assertEquals(422333, numMoves);
-
-        numMoves = generateMovesCount(board, 5);
-        assertEquals(15833292, numMoves);
+        int[] expectedMoves = {6, 264, 9467, 422333, 15833292, 706045033};
+        for (int i = 1; i <= 6; i++) {
+            numMoves = generateMovesCount(board, i);
+            assertEquals(expectedMoves[i - 1], numMoves);
+        }
     }
 
     @Test
     void testMoveGenerationPosition5() {
         Board board = FenService.createGameFromFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ -");
         long numMoves;
-
-        numMoves = generateMovesCount(board, 1);
-        assertEquals(44, numMoves);
-
-        numMoves = generateMovesCount(board, 2);
-        assertEquals(1486, numMoves);
-
-        numMoves = generateMovesCount(board, 3);
-        assertEquals(62379, numMoves);
-
-        numMoves = generateMovesCount(board, 4);
-        assertEquals(2103487, numMoves);
-
-        numMoves = generateMovesCount(board, 5);
-        assertEquals(89941194, numMoves);
+        int[] expectedMoves = {44, 1_486, 62_379, 2_103_487, 89_941_194};
+        for (int i = 0; i < expectedMoves.length; i++) {
+            numMoves = generateMovesCount(board, i + 1);
+            assertEquals(expectedMoves[i], numMoves);
+        }
     }
 
     @Test
     void testMoveGenerationPosition6() {
         Board board = FenService.createGameFromFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - -");
         long numMoves;
-
-        numMoves = generateMovesCount(board, 1);
-        assertEquals(46, numMoves);
-
-        numMoves = generateMovesCount(board, 2);
-        assertEquals(2079, numMoves);
-
-        numMoves = generateMovesCount(board, 3);
-        assertEquals(89890, numMoves);
-
-        numMoves = generateMovesCount(board, 4);
-        assertEquals(3894594, numMoves);
+        int[] expectedMoves = {46, 2_079, 89_890, 3_894_594, 164_075_551};
+        for (int i = 0; i < expectedMoves.length; i++) {
+            numMoves = generateMovesCount(board, i + 1);
+            assertEquals(expectedMoves[i], numMoves);
+        }
     }
 }
