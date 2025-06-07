aadfcbe571b51a0bdcc342f194cc21a534af14e0
Catalin <catalin.mandru@stud.ubbcluj.ro>
2025-06-07 17:47:39 +0300

    Clean front

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
 import { TranslocoPipe } from '@jsverse/transloco';
-import { HeaderComponent } from '../core/header/header.component';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenubarModule, TranslocoPipe, HeaderComponent, ToastModule],
+  imports: [RouterOutlet, MenubarModule, TranslocoPipe, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
